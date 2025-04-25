393dd4fa0961e2b2c1b95536a93d1a73b9b6b1f4
turboyjtx <turboyjtx@gmail.com>
2025-04-25 14:44:36 +0800

    5.0.4

--- TurboSDK.framework/Headers/TurboRequest.h
+++ TurboSDK.framework/Headers/TurboRequest.h
@@ -12,17 +12,13 @@ NS_ASSUME_NONNULL_BEGIN
 @interface TurboRequest : WindMillAdRequest
 
 
-
 + (id)requestWithPlacementId:(NSString *)placementId;
-
 
 /// 【可选】，媒体用户ID。
 @property (nonatomic,copy) NSString *userId;
 
 /// 【可选】，服务端激励回传时，透传参数。
 @property (nonatomic,strong) NSDictionary<NSString *, NSString *> *options;
-
-
 
 
 @end
