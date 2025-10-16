09f175d281fa86a91e5d9fbb4423d68f6e4f332c
turboyjtx <turboyjtx@gmail.com>
2025-10-16 14:49:29 +0800

    2025双11版本

--- TurboSDK.framework/Headers/TurboRequest.h
+++ TurboSDK.framework/Headers/TurboRequest.h
@@ -5,16 +5,15 @@
 //  Created by mac on 2024/11/21.
 //
 
+#import <WindMillSDK/WindMillSDK.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface TurboRequest:NSObject
+@interface TurboRequest : WindMillAdRequest
 
 
 + (id)requestWithPlacementId:(NSString *)placementId;
 
-/// 【必传】，广告位ID。
-@property (nonatomic,copy) NSString *placementId;
 /// 【可选】，媒体用户ID。
 @property (nonatomic,copy) NSString *userId;
 
--- TurboSDK.framework/Headers/TurboSDK.h
+++ TurboSDK.framework/Headers/TurboSDK.h
@@ -5,22 +5,13 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <WindMillSDK/WindMillSDK.h>
-#import "TurboRequest.h"
-#import "TurboSplashAdDelegate.h"
-#import "TurboRewardVideoAdDelegate.h"
-#import "TurboInterstitialAdDelegate.h"
-#import "TurboNativeAdDelegate.h"
-#import "TurboBannerAdDelegate.h"
-#import "TurboRequest.h"
+#import <TurboSDK/TurboRequest.h>
 
 @interface TurboSDK : NSObject
 
 + (instancetype)sharedInstance;
 
 + (NSString *)SDKVersion;
-
-//是否启用日志。日志默认关闭，设置为YES时日志打开
-@property (class, nonatomic, assign) BOOL logEnabled;
 
 // 初始化sdk
 + (void)initSDKWithAppId:(NSString *)appid;
@@ -30,19 +21,11 @@
 
 
 // 开屏
-/**
- *  广告发起请求并展示在Window中
- *  详解：当bottomView不为空，广告以半屏形式展示在传入的Window的上半部，剩余部分展示传入的bottomView
- *       请注意bottomView需设置好宽高，所占的空间不能过大，并保证广告界面的高度大于360。
- *       当bottomView为空，广告以全屏形式展示在传入的Window中
- *  @param bottomView [可选] 自定义底部View，可以在此View中设置应用Logo，
- *
- */
-- (void)loadAndShowSplashWithPlacementId:(NSString *)placementId withBottomView:(UIView *)bottomView withDelegate:(id<TurboSplashAdDelegate>)delegate;
+- (void)loadAndShowSplashWithPlacementId:(NSString *)placementId withBottomView:(UIView *)bottomView withDelegate:(id<WindMillSplashAdDelegate>)delegate;
 
 
 // 插屏
-- (void)loadIntersititialWithPlacementId:(NSString *)placementId withDelegate:(id<TurboInterstitialAdDelegate>)delegate;
+- (void)loadIntersititialWithPlacementId:(NSString *)placementId withDelegate:(id<WindMillIntersititialAdDelegate>)delegate;
 
 - (BOOL)checkIntersititialRead;
 
@@ -50,23 +33,24 @@
 
 
 // 激励
-
-- (void)loadRewardVideoWithRequest:(TurboRequest *)request withDelegate:(id<TurboRewardVideoAdDelegate>)delegate;
+- (void)loadRewardVideoWithRequest:(WindMillAdRequest *)request WithRewardVideoAd:(WindMillRewardVideoAd *)rewardVideoAd;
 
 - (BOOL)checkRewardRead;
+
 
 - (void)showRewardWithRootViewController:(UIViewController *)vc withOptions:(NSDictionary *)option;
 
 
 // 信息流
-- (void)loadNativeWithPlacementId:(NSString *)placementId withAdSize:(CGSize )adSize withDelegate:(id<TurboNativeAdDelegate>)delegate;
+- (void)loadNativeWithPlacementId:(NSString *)placementId withAdSize:(CGSize )adSize withDelegate:(id<WindMillNativeAdsManagerDelegate>)delegate;
 
-- (UIView *)showNativeViewWithUIViewController:(UIViewController *)vc;
+- (UIView *)showNativeViewWithUIViewController:(UIViewController *)vc withDelegate:(id<WindMillNativeAdViewDelegate>)delegate;
 
+- (NSArray *)getAllNativeAds;
 
 
 // 横幅
-- (UIView *)loadBannerWithPlacementId:(NSString *)placementId withDelegate:(id<TurboBannerAdDelegate>)delegate withRootViewController:(UIViewController *)vc;
+- (UIView *)loadBannerWithPlacementId:(NSString *)placementId withDelegate:(id<WindMillBannerViewDelegate>)delegate withRootViewController:(UIViewController *)vc;
 
 
 @end
